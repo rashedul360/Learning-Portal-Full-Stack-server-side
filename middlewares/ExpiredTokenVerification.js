64d43eec6a8728fca6e9dd5d548d8bccc6e5d15b
Rashedul islam <rashedul360rs@gmail.com>
2025-05-27 18:20:04 +0600

    Update ExpiredTokenVerification.js

--- middlewares/ExpiredTokenVerification.js
+++ middlewares/ExpiredTokenVerification.js
@@ -3,10 +3,9 @@ var jwt = require("jsonwebtoken");
 const expireTokenVerification = (req, res, next) => {
  const { token } = req.headers || "";
  const splitToken = token?.split("Bearer")[1];
- console.log(splitToken);
  jwt.verify(splitToken, "shhhhh", function (err) {
   if (err) {
-   console.log(err);
+   console.log(`erorr on expire token verify ${err}`);
    return res.status(401).json({ message: "token expired", status: 401 });
   }
   return next(err);
